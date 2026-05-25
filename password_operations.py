112ee50fb8f32f7faa522ca3a55d81fdd63e06c6
Bathka Sowmya <bathkasowmya@gmail.com>
2026-05-25 14:04:13 +0530

    Added special characters option for password generation

--- password_operations.py
+++ password_operations.py
@@ -14,11 +14,15 @@ def generate_password():
     length = int(length)
 
     include_numbers=input("Include numbers? (yes/no):").strip().lower()
+    include_special=input("Include special characters? (yes/no):").strip().lower()
 
     password_characters = string.ascii_letters
 
     if include_numbers == "yes":
         password_characters += string.digits
+
+    if include_special == "yes":
+        password_characters += string.punctuation
 
     password = ""
 
